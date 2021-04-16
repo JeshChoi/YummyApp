a85c6c5525d86adeef262e21789f8e6d264b5191
Jomango2003 <joshua.choi2003@gmail.com>
2021-04-16 14:09:38 -0700

    cheems brrrrrr

--- app/src/main/java/com/example/yummyapp/MainActivity.java
+++ app/src/main/java/com/example/yummyapp/MainActivity.java
@@ -9,9 +9,13 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.Context;
+import android.view.View;
 
 import com.example.yummyapp.adapter.MovieListAdapter;
 import com.example.yummyapp.model.MovieModel;
@@ -24,11 +28,23 @@ public class MainActivity extends AppCompatActivity {
     private List<MovieModel> movieModelList;
     private MovieListAdapter adapter;
     private MovieListViewModel viewModel;
+    public Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity_2.class);
+                startActivity(intent);
+            }
+        });
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         final TextView noResult = findViewById(R.id.noResultTv);
